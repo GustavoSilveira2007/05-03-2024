8031a29964c4dc4b1a7fb813eb24321c293d78e5
GUSTAVO DA ROSA SILVEIRA <02981158082@senacrs.edu.br>
2024-03-05 11:39:22 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("O GREMIO VAI SAIR CAMPEAO E O INTER NÃO!!!")
+
+
+const num1 = 20;
+const num2 = 654;
+
+let soma = num1 + num2;
+
+console.log("O valor da soma é " + soma + " - daleeeeee gremio")
